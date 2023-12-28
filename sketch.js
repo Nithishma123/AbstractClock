a818aa8ccce3a990f90d84523d60ade547836f01
Nithishma123 <62936146+Nithishma123@users.noreply.github.com>
2023-12-27 20:17:00 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,19 +20,19 @@ function draw() {
   }
 
   translate(width / 2, height / 2);
-  let hAngle = map(h % 12 + m/60, 0, 12,0, TWO_PI);
+  let hAngle = map(h % 12 + m / 60, 0, 12, -PI / 2, TWO_PI - PI / 2);
   rotate(hAngle);
   fill(173, 216, 230);
   rect(-50,-5,100,10);
 
-  let mAngle = map(m + s / 60, 0, 60, 0, TWO_PI);
+  let mAngle = map(m + s / 60, 0, 60, -PI / 2, TWO_PI - PI / 2);
   rotate(-hAngle);
   rotate(mAngle);
   fill(111, 143, 175);
   rect(-75,-3,150,6);
 
   let tick = s + (frameCount % 60) / 60;
-  let sAngle = map(tick, 0, 60,0, TWO_PI);
+  let sAngle = map(tick, 0, 60, -PI / 2, TWO_PI - PI / 2);
   rotate(-mAngle);
   rotate(sAngle);
   fill(0, 0, 128);
@@ -41,5 +41,5 @@ function draw() {
 
 function updateBackground() {
   bgColor = [random(255), random(255), random(255)];
-  background(bgColor[0], bgColor[1], bgColor[2]);
+  background(bgColor[0] , bgColor[1], bgColor[2]);
 }
