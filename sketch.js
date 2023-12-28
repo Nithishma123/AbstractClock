2ae214a2dc474e8dd0ebaa28f2e67910dbf895a8
Nithishma123 <62936146+Nithishma123@users.noreply.github.com>
2023-12-27 19:59:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-let lastMinuteChange = 0;
-let backgroundColors = [];
+let lastMinChange = 0;
+let bgColor = [];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800,60);
 }
 
 function draw() {
@@ -10,42 +10,36 @@ function draw() {
   let m = minute();
   let s = second();
 
-  // Check if a minute has passed
-  if (m !== lastMinuteChange) {
-    // Update background color every minute
+  if(m  !== lastMinChange) {
+    console.log('Minute changed:', m);
     updateBackground();
-    lastMinuteChange = m;
-  } else {
-    // Clear the background to prevent traces
-    background(backgroundColors[0], backgroundColors[1], backgroundColors[2]);
+    lastMinChange = m;
+  }
+  else {
+    background(bgColor[0], bgColor[1], bgColor[2]);
   }
 
   translate(width / 2, height / 2);
-
-  // Draw hour hand
-  let hAngle = map(h % 12 + m / 60, 0, 12, 0, TWO_PI);
+  let hAngle = map(h % 12 + m/60, 0, 12,0, TWO_PI);
   rotate(hAngle);
-  fill(255, 0, 0);
-  rect(-50, -5, 100, 10);
+  fill(173, 216, 230);
+  rect(-50,-5,100,10);
 
-  // Draw minute hand
   let mAngle = map(m + s / 60, 0, 60, 0, TWO_PI);
-  rotate(-hAngle); // Compensate for the hour hand rotation
+  rotate(-hAngle);
   rotate(mAngle);
-  fill(0, 255, 0);
-  rect(-75, -3, 150, 6);
+  fill(111, 143, 175);
+  rect(-75,-3,150,6);
 
-  // Draw smooth ticking seconds hand
-  let smoothSeconds = s + (frameCount % 60) / 60;
-  let sAngle = map(smoothSeconds, 0, 60, 0, TWO_PI);
-  rotate(-mAngle); // Compensate for the minute hand rotation
+  let tick = s + (frameCount % 60) / 60;
+  let sAngle = map(ticl, 0, 60, TWO_PI);
+  rotate(-mAngle);
   rotate(sAngle);
-  fill(0, 0, 255);
+  fill(0, 0, 128);
   rect(-100, -2, 200, 4);
 }
 
 function updateBackground() {
-  // Update background color
-  backgroundColors = [random(255), random(255), random(255)];
-  background(backgroundColors[0], backgroundColors[1], backgroundColors[2]);
+  bgColor = [random(255), random(255), random(255)];
+  background(bgColor[0], , bgColor[1], bgColor[2]);
 }
